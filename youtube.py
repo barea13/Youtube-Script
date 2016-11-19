454cc0f8648893ae29964029027caa1b09eae334
Barea <barea1396@gmail.com>
2016-11-19 13:30:19 +0100

    Source
    This script open urls every 5 seconds. You can use it whatever you want.

---
+++ youtube.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+# Barea's Code
+
+# Módulos
+import webbrowser
+import time
+# Constantes
+
+# Clases
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+
+# Funciones
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+
+def main():
+    url_link = input('\nIntroduzca una URL de youtube: ')
+    loop = input('\n¿Cuantas veces quieres que se repita? ')
+    loop = int(loop)
+    porcentaje = 100/loop
+    porcentaje_2 = porcentaje
+    print('\nProgreso:')
+    for i in range(loop):
+        webbrowser.Mozilla
+        webbrowser.Chrome
+        print('\r',porcentaje, '%')
+        porcentaje = porcentaje + porcentaje_2
+        webbrowser.open(url_link, new=1, autoraise=True)
+
+        time.sleep(5)
+# Programa principal
+# ---------
+
+print('\n ¡¡Bienvenido al script para Youtube @ N3utr0n!!')
+main()
